0977ecf235d59a8cf503059fe6507c4129111b91
Malek F <43182938+MalekFarag@users.noreply.github.com>
2020-03-23 15:39:31 -0400

    completed createProd (need delete function)

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
-<?php
-require_once 'load.php';
+<?php 
+    require_once '../load.php';
+    confirm_logged_in();
+    comfirm_verified();
 
-
-if (isset($_GET['filter'])) {
-    //Filter is not working
-    $args = array(
-        'tbl' => 'tbl_products',
-        'tbl2' => 'tbl_category',
-        'tbl3' => 'tbl_prod_category',
-        'col' => 'prod_id',
-        'col2' => 'category_id',
-        'col3' => 'category_name',
-        'filter' => $_GET['filter'],
-    );
-    $getProds = getProdsByCategory($args);
-} else {
-    $prod_table = 'tbl_products';
-    $getProds = getAll($prod_table);
-}
-
-
-?>
+    if (isset($_GET['filter'])) {
+        //Filter is not working
+        $args = array(
+            'tbl' => 'tbl_products',
+            'tbl2' => 'tbl_category',
+            'tbl3' => 'tbl_prod_category',
+            'col' => 'prod_id',
+            'col2' => 'category_id',
+            'col3' => 'category_name',
+            'filter' => $_GET['filter'],
+        );
+        $getProds = getProdsByCategory($args);
+    } else {
+        $prod_table = 'tbl_products';
+        $getProds = getAll($prod_table);
+    }
+    
+    
+    ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -28,25 +29,36 @@ if (isset($_GET['filter'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome to the SportCheck CMS!</title>
+    <title>Admin Dashboard</title>
 </head>
 <body>
-    <?php include 'templates/header.php';?>
+    <h1>Welcome, <?php echo $_SESSION['user_name']; ?></h1><br>
 
-    <h1>Welcome to SportCheck CMS</h1>
+    <h3>User Settings</h3>
+    <ul>
+    <a href="admin_createuser.php">Create Account</a>
+    <a href="admin_edituser.php">Edit Current Account</a>
+    <a href="admin_logout.php">Logout</a>
+    </ul>
 
+    <!-- link to edit product** PAGE NEEDS UPDATING (sql queries to work with linking tables) -->
+    <a href="admin_createproduct.php">Create New Product</a>
+
+    <h2>Products</h2>
+    
     <!-- NEED SEARCH BAR -->
 
     <?php while ($row = $getProds->fetch(PDO::FETCH_ASSOC)): ?>
-        <a href="prodDetails.php?id=<?php echo $row['prod_id']; ?>">
-        <div class="prodItem">
-            <img src="images/shoes/<?php echo $row['image']; ?>" alt="<?php echo $row['name']; ?>" />
+        <div class="prodItem editProduct">
+            <img src="../images/shoes/<?php echo $row['image']; ?>" alt="<?php echo $row['name']; ?>" />
             <h5><?php echo $row['price']; ?></h5>
             <h2><?php echo $row['name']; ?></h2>
         </div>
-        </a>
+        <!-- link to edit product** PAGE NEEDS UPDATING -->
+        <a href="admin_editproduct.php?id=<?php echo $row['prod_id']; ?>">Edit Product</a>
+        <!-- link to delete product** NEEDS NEW PAGE -->
+        <a href="admin_deleteproduct.php?id=<?php echo $row['prod_id']; ?>">Delete Product</a>
     <?php endwhile; ?>
-
-    <?php include 'templates/footer.php';?>
+    
 </body>
 </html>
