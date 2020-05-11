ba2b630869de432520965f8f379084fce2b85ca0
Malek F <43182938+MalekFarag@users.noreply.github.com>
2020-05-11 14:27:45 -0400

    Update user.php

--- admin/scripts/user.php
+++ admin/scripts/user.php
@@ -29,11 +29,11 @@ function createuser($fname, $username, $password, $email){
         $mail->Port = 465;
         $mail->SMTPAuth=true;
 
-        $mail->Username='faragmalek14@gmail.com';
-        $mail->Password='Malooky14'; // please dont steal my password. I really dont want to change it
+        $mail->Username='company@gmail.com';
+        $mail->Password='company'; // please dont steal my password. I really dont want to change it
 
         $mail->addAddress($email);
-        $mail->setFrom('faragmalek14@gmail.com');
+        $mail->setFrom('company@gmail.com');
         
 
         $mail->isHTML(true);
